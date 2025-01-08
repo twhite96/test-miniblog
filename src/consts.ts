bd19be03458a84ca04d06747e69baaf0f9f9a936
tiff <tiff@tifflabs.org>
2025-01-07 21:37:47 -0500

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://micro.tiff.engineer";
+export const SITE_TITLE = "yak shaving with tiff on software";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "Small, opinionated thoughts on softare engineering and my journey through it.";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "contact@tifflabs.org";
